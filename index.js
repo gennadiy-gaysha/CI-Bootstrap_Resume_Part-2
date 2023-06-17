5bc9e851d135b0d3621626e6ef868788a32b13ee
gennadiy-gaysha <gennadiy-gaysha@gmail.com>
2023-06-17 18:33:01 +0200

    add markers to the map in index.js file of Gosha_Dudar folder

--- index.js
+++ index.js
@@ -1,33 +1,82 @@
+// function initMap() {
+//   const map = new google.maps.Map(document.getElementById("map"), {
+//     zoom: 3,
+//     center: { lat: 46.619261, lng: -33.134766 },
+//   });
+//   //   const infoWindow = new google.maps.InfoWindow({
+//   //     content: "",
+//   //     disableAutoPan: true,
+//   //   });
+//   // Create an array of alphabetical characters used to label the markers.
+//   const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//   // Add some markers to the map.
+//   //   const markers = locations.map((position, i) => {
+//   //     const label = labels[i % labels.length];
+//   //     const marker = new google.maps.Marker({
+//   //       position,
+//   //       label,
+//   //     });
+
+//   //     // markers can only be keyboard focusable when they have click listeners
+//   //     // open info window when marker is clicked
+//   //     marker.addListener("click", () => {
+//   //       infoWindow.setContent(label);
+//   //       infoWindow.open(map, marker);
+//   //     });
+//   //     return marker;
+//   //   });
+
+//   //   // Add a marker clusterer to manage the markers.
+//   //   new MarkerClusterer({ markers, map });
+//   const locations = [
+//     { lat: 40.785091, lng: -73.968285 },
+//     { lat: 41.084045, lng: -73.874245 },
+//     { lat: 40.754932, lng: -73.984016 },
+//   ];
+
+//   // Add some markers to the map.
+//   const markers = locations.map(function (location, i) {
+//     return new google.maps.Marker({
+//       position: location,
+//       label: labels[i % labels.length],
+//     });
+//   });
+
+//   // Add a marker clusterer to manage the markers.
+//   new MarkerClusterer({ markers, map });
+// }
+
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 3,
-    center: { lat: 46.619261, lng: -33.134766 },
+    center: { lat: -28.024, lng: 140.887 },
   });
-  //   const infoWindow = new google.maps.InfoWindow({
-  //     content: "",
-  //     disableAutoPan: true,
-  //   });
-  //   // Create an array of alphabetical characters used to label the markers.
-  //   const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  //   // Add some markers to the map.
-  //   const markers = locations.map((position, i) => {
-  //     const label = labels[i % labels.length];
-  //     const marker = new google.maps.Marker({
-  //       position,
-  //       label,
-  //     });
+  const infoWindow = new google.maps.InfoWindow({
+    content: "",
+    disableAutoPan: true,
+  });
+  // Create an array of alphabetical characters used to label the markers.
+  const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  // Add some markers to the map.
+  const markers = locations.map((position, i) => {
+    const label = labels[i % labels.length];
+    const marker = new google.maps.Marker({
+      position,
+      label,
+    });
 
-  //     // markers can only be keyboard focusable when they have click listeners
-  //     // open info window when marker is clicked
-  //     marker.addListener("click", () => {
-  //       infoWindow.setContent(label);
-  //       infoWindow.open(map, marker);
-  //     });
-  //     return marker;
-  //   });
+    // markers can only be keyboard focusable when they have click listeners
+    // open info window when marker is clicked
+    marker.addListener("click", () => {
+      infoWindow.setContent(label);
+      infoWindow.open(map, marker);
+    });
+    return marker;
+  });
 
-  //   // Add a marker clusterer to manage the markers.
+  // Add a marker clusterer to manage the markers.
   //   new MarkerClusterer({ markers, map });
+  const markerCluster = new markerClusterer.MarkerClusterer({ map, markers });
 }
 
 const locations = [
